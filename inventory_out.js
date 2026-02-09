1bd1ec798d69c5cf2ec0a976fed0813b9981fab1
boulosapthayla-byte <boulos.apthayla@gmail.com>
2026-02-09 12:50:20 -0300

    Update inventory_out.js

--- inventory_out.js
+++ inventory_out.js
@@ -1,1 +1,11 @@
+/* --- FUNÇÃO DE NAVEGAÇÃO --- */
 
+function voltarAoHub() {
+    // Redireciona o navegador de volta para a tela principal
+    window.location.href = "section_hub.html";
+}
+
+/* ----------------------------------------------------------- 
+   DICA: Se no futuro você quiser adicionar lógica (tipo calcular custos),
+   você escreve ABAIXO dessa função. Por enquanto, só isso basta!
+----------------------------------------------------------- */
